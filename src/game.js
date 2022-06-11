e98974c6a18c9ba10bb3636b577a2e62297ebeaf
Di <d.osseyran@hotmail.com>
2022-06-11 15:56:51 +0200

    iteration 1 so far

--- src/game.js
+++ src/game.js
@@ -1,6 +1,11 @@
 class Game {
   drawGrid() {
+    for (let i = 0; i < 11; i++) {
+      line(0, SQUARE_SIDE * i, WIDTH, SQUARE_SIDE * i);
+      line(SQUARE_SIDE * i, 0, SQUARE_SIDE * i, HEIGHT);
+    }
     // Iteration 1
+
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
   }
---
+++ src/player.js
@@ -0,0 +1,5 @@
+class Player {
+  constructor(col, row) {
+    (this.col = col), (this.row = row);
+  }
+}
